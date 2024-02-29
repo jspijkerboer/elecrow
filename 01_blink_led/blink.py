1051d533106ed5f884a95879f3ef9a5a0aab987f
Jeff <jspijkerboer@outlook.com>
2024-02-29 22:01:16 +0100

    blink, introduced some variance in timing

--- 01_blink_led/blink.py
+++ 01_blink_led/blink.py
@@ -4,17 +4,13 @@ import RPi.GPIO as GPIO
 led_pin = 17
 
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(led_pin, GPIO.OUT)
 
 try:
     while True:
         GPIO.output(led_pin, GPIO.HIGH)
-
-        time.sleep(0.2)
-
+        time.sleep(0.6)
         GPIO.output(led_pin, GPIO.LOW)
-
-        time.sleep(0.2)
+        time.sleep(0.1)
 except KeyboardInterrupt:
     GPIO.cleanup()
